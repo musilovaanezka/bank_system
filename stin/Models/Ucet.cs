94dcc31683106f67e646f816ce5683325560ad9f
Musilová <amusil@goldbeck.biz>
2023-04-18 15:42:57 +0200

    database changes

---
+++ stin/Models/Ucet.cs
@@ -0,0 +1,13 @@
+﻿namespace stin.Models
+{
+    public class Ucet
+    {
+        public int Id { get; set; }
+
+        public string UcetNum { get; set; }
+
+        public string Mena { get; set; }
+
+        public int hodnota { get; set; }
+    }
+}
