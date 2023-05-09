de79c55448b99d8c1252540f84852e549593bd1e
Musilová <amusil@goldbeck.biz>
2023-05-09 19:31:47 +0200

    základ pro main page

--- stin/Controllers/AuthenticateController.cs
+++ stin/Controllers/AuthenticateController.cs
@@ -19,6 +19,7 @@ namespace stin.Controllers
         private readonly IEmailService _emailService;
         private readonly IConfiguration _configuration;
         private readonly ApplicationDbContext _context;
+        private string username;
 
         public AuthenticateController(
             //UserManager<IdentityUser> klientManager,
@@ -40,6 +41,16 @@ namespace stin.Controllers
             return View();
         }
 
+        public IActionResult EmailOdeslan()
+        {
+            return View();
+        }
+
+        public IActionResult RedirectToHomePage(string username) {
+            TempData["name"] = username;
+            return RedirectToAction("UsersPage", "HomePage");
+        }
+
         [HttpPost]
         public IActionResult Login(string username, string password)
         {
@@ -57,62 +68,62 @@ namespace stin.Controllers
 
             //var token = await _klientManager.GenerateTwoFactorTokenAsync(identityUser, "Email");
 
-            var url = Url.Action("LoginFromEmail");
-            var url2 = HttpContext.Request.GetEncodedUrl();
-            var a = @"<link>https://localhost:44354/Authenticate/LoginFromEmail?username=" + user.Username + "</link>";
+            var url = Url.Action(action: "LoginFromEmail", controller: "Authenticate", values: new {name = username}, protocol: "https", host: "localhost:44354");
+            //var url2 = HttpContext.Request.GetEncodedUrl();
+            var a = @"<link>https://localhost:44354/Authenticate/LoginFromEmail/" + user.Username.ToString() + "</link>";
             
 
             // TODO - vyměnit můj email za user.Username
-            var message = new Message(new string[] { user.Username }, "Ověřovací kód", "Váš ověřovací link je:", a);
+            var message = new Message(new string[] { user.Username }, "Ověřovací kód", "Váš ověřovací link je:", url);
             
             _emailService.SendEmail(message);
 
-            return Ok(); 
+            return RedirectToAction("EmailOdeslan"); 
         }
 
-        [HttpPost]
-        public IActionResult LoginFromEmail([FromQuery(Name = "username")] string username)
+        [HttpGet]
+        public IActionResult LoginFromEmail(string name)
         {
             var user = _context.Klienti
-                           .Where(e => e.Username == username)
-                           .FirstOrDefault(); 
+                           .Where(e => e.Username == name)
+                           .FirstOrDefault();
             //var signIn = _signInManager.TwoFactorSignInAsync("Email", code, false, false);
-           
+
             if (user != null)
             {
-                    var authClaims = new List<Claim>
-                    {
-                        new Claim(ClaimTypes.Name, user.Username),
-                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-                    };
-                    
-                    var jwtToken = GetToken(authClaims);
+                //var authClaims = new List<Claim>
+                //    {
+                //        new Claim(ClaimTypes.Name, user.Username),
+                //        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                //    };
 
-                    return Ok(new
-                    {
-                        token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
-                        expiration = jwtToken.ValidTo
-                    });
+                //var jwtToken = GetToken(authClaims);
+                //var stringToken = new JwtSecurityTokenHandler().WriteToken(jwtToken);
+                //RouteValueDictionary dict = new RouteValueDictionary();
+                //dict.Add("username", username);
+                this.username = name;
+                return RedirectToAction("RedirectToHomePage", new {username = name});
             }
 
-            return NotFound($"Neplatný´ověřovací kód");
+            return NotFound("Neplatný´ověřovací kód");
 
         }
 
-        private JwtSecurityToken GetToken(List<Claim> authClaims)
-        {
-            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+        //private JwtSecurityToken GetToken(List<Claim> authClaims)
+        //{
+        //    var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
 
-            var token = new JwtSecurityToken(
-                issuer: _configuration["JWT:ValidIssuer"],
-                audience: _configuration["JWT:ValidAudience"],
-                expires: DateTime.Now.AddHours(3),
-                claims: authClaims,
-                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
-                );
+        //    var token = new JwtSecurityToken(
+        //        issuer: _configuration["JWT:ValidIssuer"],
+        //        audience: _configuration["JWT:ValidAudience"],
+        //        expires: DateTime.Now.AddHours(3),
+        //        claims: authClaims,
+        //        signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        //        );
             
-            return token;
-        }
+
+        //    return token;
+        //}
 
 
         //[HttpGet]
---
+++ stin/Controllers/HomePageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using stin.Models;
+using stin.Data;
+
+namespace stin.Controllers
+{
+    public class HomePageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomePageController(
+            ApplicationDbContext context
+        )
+        {
+            _context = context;
+        }
+
+
+        public IActionResult UsersPage()
+        {
+            var username = TempData["name"] as string;
+            var user = _context.Klienti
+               .Where(e => e.Username == username)
+               .FirstOrDefault();
+            ViewBag.Klient = user;
+            return View();
+        }
+
+
+    }
+}
